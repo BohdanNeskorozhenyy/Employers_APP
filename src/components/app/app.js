406cfe1d8d904115fab1b77b76c8c044d8c6933b
Bohdan <bogdaneskor@gmail.com>
2021-11-29 14:06:52 +0200

    add filter and rearch functional

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,9 @@ class App extends Component {
             { name: "John S.", salary: 400, increase: true, rise: true, id: 1 },
             { name: "Alex M.", salary: 3000, increase: false, rise: false, id: 2 },
             { name: "Carl W.", salary: 5000, increase: false, rise: false, id: 3 }
-         ]
+         ],
+         term:'',
+         filter:'all'
       }
       this.maxId = 4
    }
@@ -55,17 +57,44 @@ class App extends Component {
          })
       }))
    }
+   searchEmp = (items, term) =>{
+      if(term.length === 0){
+         return items
+      }
+       return items.filter(item => {
+          return item.name.indexOf(term) > -1
+       })
+   }
+   filterPost = (items, filter) =>{
+      switch(filter){
+         case 'rise':
+            return items.filter(item => item.rise);
+         case 'moreThen1000':
+            return items.filter(item => item.salary > 1000);
+         default:
+            return items
+      }
+   }
+
+   onUpdateSearch = (term) =>{
+      this.setState({term: term})
+   }
+   onUpdataFilter = (filter) =>{
+      this.setState({filter:filter})
+   }
    render() {
+      const{data,term, filter} = this.state;
       const emploees = this.state.data.length;
       const increased = this.state.data.filter(item => item.increase).length;
+      const visibleData = this.filterPost(this.searchEmp(data, term),filter);
       return (
          <div className="app">
             <AppInfo emploees={emploees} increased={increased}/>
             <div className="search-panel">
-               <SearchPanel />
-               <AppFilter />
+               <SearchPanel onUpdateSearch={this.onUpdateSearch} />
+               <AppFilter onUpdataFilter={this.onUpdataFilter} filter={filter}/>
             </div>
-            <EmployersList data={this.state.data}
+            <EmployersList data={visibleData}
                onDelete={this.deleteItem}
                onToggleProp={this.onToggleProp}/>
             <EmployersAddForm onAdd={this.addItem} />
